02ffe25ecde53fb5bf3ad8d4fd91a341de02128b
king22 <henrytan.next@gmail.com>
2019-09-11 20:13:48 +0700

    support function to download geoip db

--- lib/geoip.ex
+++ lib/geoip.ex
@@ -1,3 +1,25 @@
 defmodule GeoIP do
+  require Logger
+  import GunEx, only: [
+    http_request: 6,
+    get_body: 1
+  ]
 
+  def download_geoLite() do
+    url = "https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz"
+    headers = %{}
+    http_request("GET", url, headers, "", proxy_option(), nil)
+  end
+
+  defp proxy_option() do
+    default_opts =
+      %{
+        retry: 0,
+        recv_timeout: 25000,
+        connect_timeout: 35000,
+        retry_timeout: 5000,
+        transport_opts: [{:reuseaddr, true}, {:reuse_sessions, false}, {:linger, {false, 0}}]
+      }
+    default_opts
+  end
 end
