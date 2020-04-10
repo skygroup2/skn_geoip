5917834c805e47f9662630efd57730c58c7aec70
king22 <henrytan.next@gmail.com>
2020-04-10 16:08:36 +0700

    Update deps

--- mix.exs
+++ mix.exs
@@ -15,14 +15,14 @@ defmodule SknGeoip.MixProject do
   def application do
     [
       mod: {GeoIP, []},
-      extra_applications: [:mnesia, :logger, :logger_lager_backend, :ssh, :gun, :gen_statem2]
+      extra_applications: [:mnesia, :logger, :logger_lager_backend, :ssh, :gun]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:logger_lager_backend, git: "https://github.com/gskynet/logger_lager_backend.git", branch: "master"},
+      {:logger_lager_backend, git: "https://github.com/Subhuti20/logger_lager_backend.git", branch: "master"},
       {:skn_lib, git: "git@gitlab.com:gskynet_lib/skn_lib.git", branch: "master"},
       {:skn_proto, git: "git@gitlab.com:gskynet_lib/skn_proto.git", branch: "master"},
       {:lager, "~> 3.8", override: true}
